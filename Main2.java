56f9c58b8130d897e3fc290d4c4fbd4c8e8ffb1c
Enzo <81955852+Enzooo22@users.noreply.github.com>
2026-03-12 22:21:24 +0800

    Update Main2.java

--- Main2.java
+++ Main2.java
@@ -3,20 +3,19 @@ public class Main {
 public static void main(String[] args) {
 Scanner sc = new Scanner(System.in);
 
-String[] students = {"Dwight", "Jexkean", "Christian", "Kyle", "Chester", 
-                             "Kiel", "Russel", "Paul", "Joseph", "Seanjay"};
+String[] students = {"Dwight", "Jexkean", "Christian", "Kyle", "Chester", "Kiel", "Russel", "Paul", "Joseph", "Seanjay"};
 double[] grades = new double[students.length];
 
 System.out.println("Enter grades for each student:");
 for (int i = 0; i < students.length; i++) {
-      System.out.print(students[i] + ": ");
-      grades[i] = sc.nextDouble();
-      }
+System.out.print(students[i] + ": ");
+grades[i] = sc.nextDouble();
+}
         
 double sum = 0;
 for (double grade : grades) {
-    sum += grade;
-    }
+sum += grade;
+}
 
 double average = sum / grades.length;
 System.out.println("Average grade: " + average);
@@ -32,12 +31,12 @@ grades[j + 1] = tGrades;
 String tStudents = students[j];
 students[j] = students[j + 1];
 students[j + 1] = tStudents;
-    }
   }
+ }
 }
 
 sc.nextLine();
-System.out.print("\nEnter student name to search: ");
+System.out.print("Enter student name to search: ");
 String name = sc.nextLine();
 
 boolean found = false;
@@ -46,13 +45,13 @@ if (students[i].equalsIgnoreCase(name)) {
 System.out.println("Student: " + students[i] + " | Grade: " + grades[i]);
 found = true;
 break;
-  }
+ }
 }
 
 if (!found) {
 System.out.println("Student not found.");
-            }
+}
 
         
-    }
+ }
 }
